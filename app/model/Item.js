3df612c8e257e91854da25fafcfa076dcea52411
Daniel Sauble <djsauble@gmail.com>
2015-06-01 11:46:11 +0000

    Makes app iOS-ready with Cordova

--- app/model/Item.js
+++ app/model/Item.js
@@ -1,5 +1,9 @@
 Ext.define('TodoApp.model.Item', {
 	extend: 'Ext.data.Model',
+	requires: [
+		'Ext.data.proxy.LocalStorage',
+		'Ext.data.identifier.Uuid'
+	],
 	config: {
 		identifier: {
 			type: 'uuid'
--- app/view/Main.js
+++ app/view/Main.js
@@ -1,6 +1,7 @@
 Ext.define('TodoApp.view.Main', {
     extend: 'Ext.Panel',
     alias: 'widget.todo-main',
+    fullscreen: true,
     config: {
         layout: 'fit',
         items: { xtype: 'todo-list' }
